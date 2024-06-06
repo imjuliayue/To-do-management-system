742a664e28854e0829b335b2b318b2dadfdfe4d5
imjuliayue <juliayue6@gmail.com>
2024-06-06 15:18:32 -0400

    Added files to project

---
+++ src/main/java/com/jts/todo_management_system/TodoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jts.todo_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSystemApplication.class, args);
+	}
+
+}

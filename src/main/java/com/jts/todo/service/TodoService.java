a16a91bed8ba7a58784716fd58c90e106d43c6ab
imjuliayue <juliayue6@gmail.com>
2024-06-08 22:50:48 -0400

    Added addTodo, getTodo, getAllTodos methods.

--- src/main/java/com/jts/todo/service/TodoService.java
+++ src/main/java/com/jts/todo/service/TodoService.java
@@ -1,17 +1,45 @@
 package com.jts.todo.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
 import com.jts.todo.dto.TodoDto;
 import com.jts.todo.entity.Todo;
+import com.jts.todo.repo.TodoRepository;
+
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class TodoService {
+
+    private TodoRepository todoRepository;
+
     private ModelMapper modelMapper;
     public TodoDto addTodo(TodoDto todoDto){
-        Todo todo = modelMapper.map
-        return todoDto;
+        // Copies all the data from todoDto to todo as Todo class.
+        Todo todo = modelMapper.map(todoDto, Todo.class);
+
+        // Saves the data in this object into database.
+        todo = todoRepository.save(todo);
+
+        // Converts back to TodoDto type.
+        return modelMapper.map(todo, TodoDto.class);
+    }
+
+    public TodoDto getTodo(Long id){
+        Todo todo = todoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("TODO not found"));
+
+        return modelMapper.map(todo, TodoDto.class);
+    }
+
+    public List<TodoDto> getAllTodos(){
+        List<Todo> todos = todoRepository.findAll();
+
+        return todos.stream().map((todo) -> modelMapper.map(todo, TodoDto.class)).collect(Collectors.toList());
     }
 }
